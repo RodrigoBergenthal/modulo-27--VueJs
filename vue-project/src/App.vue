5fb128e204d631aaedd3df2e203cf2ac8dd3b864
Rodrigo Bergenthal <rodrigo_bergenthak@live.com>
2024-12-04 17:41:41 -0300

    add funcoes

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -17,6 +17,10 @@ const botaoEstaDesabilitado = false
 
 const estado = reactive({
   contador: 0,
+  email:"",
+  saldo: 5000,
+  transferindo: 0,
+
 })
 
 function incrementar () {
@@ -25,6 +29,20 @@ function incrementar () {
 
 function decrementar () {
   estado.contador --;
+}
+
+function alteraEmail (evento){
+ estado.email = evento.target.value;
+}
+
+function mostraSaldoFuturo (){
+  const {saldo , transferindo} = estado;
+  return saldo - transferindo;
+}
+
+function validaValorTransferencia (){
+  const {saldo , transferindo} = estado;
+  return saldo >= transferindo;
 }
 </script>
 
@@ -40,10 +58,28 @@ function decrementar () {
 
 <button @click='incrementar' type="button"> + </button>
 <button @click="decrementar" type="button"> - </button>
+<br/>
+<hr/>
+
+{{ estado.email }}
+<input type="email" @keyup="alteraEmail">
+<br/>
+<hr/>
+
+Saldo: {{ estado.saldo }}<br/>
+Transferindo: {{ estado.transferindo }}<br/>
+Saldo depois da Transferência: {{ mostraSaldoFuturo () }}<br/>
+<input :class="{invalido: !validaValorTransferencia()}" @keyup="evento => estado.transferindo = evento.target.value" type="number" placeholder="Quantia para transferir" />
+
 </template>
 
 <style scoped>
 img {
   max-width: 200px;
+}
+
+.invalido {
+  outline-color:red ;
+  border-color: red;
 }
 </style>
